bd166e98cbccaa6ba2548e2230cb3269a516c3d9
krishcoderGITHUB <75573226+krishcoderGITHUB@users.noreply.github.com>
2020-12-14 16:38:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,229 @@
+var player,    time_update_interval = 0;
+
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+  // create the global player from the specific iframe (#video)
+  player = new YT.Player('video', {
+events: {
+    onReady: 
+    initialize
+}
+  });
+}
+function initialize(){
+
+
+ /* // bind events
+  var playButton = document.getElementById("play-button");
+  playButton.addEventListener("click", function() {
+      player.playVideo();
+  });
+
+  var pauseButton = document.getElementById("pause-button");
+  pauseButton.addEventListener("click", function() {
+      player.pauseVideo();
+  });*/
+
+  var stopButton = document.getElementById("stop");
+  stopButton.addEventListener("click", function() {
+      player.stopVideo();
+  });
+
+var fwdbutton = document.getElementById("fast_forward");
+fwdbutton.addEventListener("click", function() {
+       var current = player.getCurrentTime();
+       var current4 = current + 15 ;
+player.seekTo(current4, true)
+});
+var rwdbutton = document.getElementById("fast_rewind");
+rwdbutton.addEventListener("click", function() {
+       var current = player.getCurrentTime();
+       var current4 = current - 15 ;
+player.seekTo(current4, true)
+});
+
+
+
+/*function initialize(){
+
+    // Update the controls on load
+    updateTimerDisplay();
+    updateProgressBar();
+
+    // Clear any old interval.
+    clearInterval(time_update_interval);
+
+    // Start interval to update elapsed time display and
+    // the elapsed part of the progress bar every second.
+    time_update_interval = setInterval(function () {
+        updateTimerDisplay();
+        updateProgressBar();
+    }, 1000);
+
+
+    $('#volume-input').val(Math.round(player.getVolume()));
+}
+
+
+// This function is called by initialize()
+function updateTimerDisplay(){
+    // Update current time text display.
+    $('#current-time').text(formatTime( player.getCurrentTime() ));
+    $('#duration').text(formatTime( player.getDuration() ));
+}
+
+
+// This function is called by initialize()
+function updateProgressBar(){
+    // Update the value of our progress bar accordingly.
+    $('#progress-bar').val((player.getCurrentTime() / player.getDuration()) * 100);
+}
+
+
+// Progress bar
+
+$('#progress-bar').on('mouseup touchend', function (e) {
+
+    // Calculate the new time for the video.
+    // new time in seconds = total duration in seconds * ( value of range input / 100 )
+    var newTime = player.getDuration() * (e.target.value / 100);
+
+    // Skip video to new time.
+    player.seekTo(newTime);
+
+});
+// Helper Functions
+
+function formatTime(time){
+    time = Math.round(time);
+
+    var minutes = Math.floor(time / 60),
+        seconds = time - minutes * 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    return minutes + ":" + seconds;
+}*/
+
+
+    // Update the controls on load
+    updateTimerDisplay();
+    updateProgressBar();
+
+    // Clear any old interval.
+    clearInterval(time_update_interval);
+
+    // Start interval to update elapsed time display and
+    // the elapsed part of the progress bar every second.
+    time_update_interval = setInterval(function () {
+        updateTimerDisplay();
+        updateProgressBar();
+    }, 1000);
+
+
+    $('#volume-input').val(Math.round(player.getVolume()));
+}
+
+
+// This function is called by initialize()
+function updateTimerDisplay(){
+    // Update current time text display.
+    $('#current-time').text(formatTime( player.getCurrentTime() ));
+    $('#duration').text(formatTime( player.getDuration() ));
+}
+
+
+// This function is called by initialize()
+function updateProgressBar(){
+    // Update the value of our progress bar accordingly.
+    $('#progress-bar').val((player.getCurrentTime() / player.getDuration()) * 100);
+}
+
+
+// Progress bar
+
+$('#progress-bar').on('mouseup touchend', function (e) {
+
+    // Calculate the new time for the video.
+    // new time in seconds = total duration in seconds * ( value of range input / 100 )
+    var newTime = player.getDuration() * (e.target.value / 100);
+
+    // Skip video to new time.
+    player.seekTo(newTime);
+
+});
+
+
+
+$('#play').on('click', function () {
+    player.playVideo();
+    $(".screenf").delay(6000).fadeOut(500); 
+    $(".share").delay(6000).fadeOut(500); 
+});
+
+
+$('#pause').on('click', function () {
+    player.pauseVideo();
+    $(".screenf").show();
+        $(".share").show();
+
+});
+
+
+// Sound volume
+
+
+$('#mute-toggle').on('click', function() {
+    var mute_toggle = $(this);
+
+    if(player.isMuted()){
+        player.unMute();
+        mute_toggle.text('volume_up');
+    }
+    else{
+        player.mute();
+        mute_toggle.text('volume_off');
+    }
+});
+
+$('#volume-input').on('change', function () {
+    player.setVolume($(this).val());
+});
+
+$('#speed').on('click', function () {
+    player.setPlaybackRate(2);
+});
+$('#speed1x').on('click', function () {
+    player.setPlaybackRate(1);
+});
+
+// To get the current playback rate for a video use this method:
+// player.getPlaybackRate()
+
+// To get all available playback rates for the current video use:
+// player.getAvailablePlaybackRates()
+
+
+
+// Helper Functions
+
+function formatTime(time){
+    time = Math.round(time);
+
+    var minutes = Math.floor(time / 60),
+        seconds = time - minutes * 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    return minutes + ":" + seconds;
+}
+
+
+$('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+});
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
